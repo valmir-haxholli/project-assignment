5a68557eb337e3d0cd79fe37729c487d05557c95
Valmir <valmir.haxholli@gmail.com>
2022-04-25 12:25:36 +0200

    project actions, action types and reducers implemented

--- src/redux/actions/projectActions.js
+++ src/redux/actions/projectActions.js
@@ -4,3 +4,78 @@ const getProjects = (projects) => ({
     type: types.GET_PROJECTS,
     payload: projects
 })
+
+const projectDeleted = () => ({
+    type: types.DELETE_PROJECT
+})
+
+const projectAdded = () => ({
+    type: types.ADD_PROJECT
+})
+
+const projectUpdated = () => ({
+    type: types.UPDATE_PROJECT
+})
+
+const getProject = (project) => ({
+    type: types.GET_SINGLE_PROJECT,
+    payload: project
+})
+
+export const loadProjects = () => {
+    return function (dispatch) {
+        axios
+            .get(`${process.env.REACT_APP_API}`)
+            .then((resp) => {
+                dispatch(getProjects(resp.data));
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+export const deleteProjects = (id) => {
+    return function (dispatch) {
+        axios
+            .delete(`${process.env.REACT_APP_API}/${id}`)
+            .then((resp) => {
+                dispatch(projectDeleted());
+                dispatch(loadProjects());
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+export const addProject = (project) => {
+    return function (dispatch) {
+        axios
+            .post(`${process.env.REACT_APP_API}`, project)
+            .then((resp) => {
+                dispatch(projectAdded());
+                dispatch(loadProjects());
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+export const getSingleProject = (id) => {
+    return function (dispatch) {
+        axios
+            .get(`${process.env.REACT_APP_API}/${id}`)
+            .then((resp) => {
+                dispatch(getProject(resp.data));
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+export const updateProject = (project, id) => {
+    return function (dispatch) {
+        axios
+            .put(`${process.env.REACT_APP_API}/${id}`, project)
+            .then((resp) => {
+                dispatch(projectUpdated());
+                dispatch(loadProjects())
+            })
+            .catch(error => console.log(error));
+    }
+}
--- src/redux/reducers/projectReducer.js
+++ src/redux/reducers/projectReducer.js
@@ -8,6 +8,29 @@ const initialState = {
 
 const projectsReducer = (state = initialState, action) => {
     switch(action.type) {
+        case types.GET_PROJECTS:
+            return {
+                ...state,
+                projects: action.payload,
+                loading: false
+            }
+        case types.GET_SINGLE_PROJECT:
+            return {
+                ...state,
+                project: action.payload,
+                loading: false
+            }
+        case types.ADD_PROJECT:
+            return {
+                ...state,
+                loading: false
+            }
+        case types.UPDATE_PROJECT:
+            return {
+                ...state,
+                loading: false
+            }
+        case types.DELETE_PROJECT:
         default:
             return state;
     }
